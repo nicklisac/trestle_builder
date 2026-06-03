cc84f6a4fe1e07cc7cc9a67c8e564a660749884a
nicklisac <lisacnick@gmail.com>
2026-06-02 23:53:17 -0400

    change gitignore to include .json

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1239577435"
+    serviceWorkerVersion: "1290999040"
   }
 });
